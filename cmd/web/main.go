5b0716e85a9094eec99fdb822ee39a0532117624
mariogarzac <mariogarzac99@gmail.com>
2023-12-22 11:02:47 -0600

    Using templ now

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -2,21 +2,30 @@ package main
 
 import (
 	"github.com/labstack/echo/v4"
-	"github.com/mariogarzac/moodeck/config"
+	"github.com/mariogarzac/moodeck/db"
+	"github.com/mariogarzac/moodeck/pkg/config"
 	"github.com/mariogarzac/moodeck/pkg/handlers"
-	"github.com/mariogarzac/moodeck/pkg/render"
 )
 
-
 var app config.AppConfig
+var portNumber = ":3000"
 
 func main(){
 
     e := echo.New()
-    e.Renderer = render.NewTemplate()
+    // e.Renderer = render.NewTemplate()
 
-    e.Static("/static", "static")
+    e.Static("/assets", "assets")
+
+    db.InitDB()
+    app.DataBase = db.GetDB()
+    defer app.DataBase.Close()
 
     repo := handlers.NewRepository(&app)
     handlers.NewHandlers(repo)
+
+
+    routes(e)
+
+    e.Logger.Fatal(e.Start(portNumber))
 }
