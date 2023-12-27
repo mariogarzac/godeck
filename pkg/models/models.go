0f0b276f767feb24bae6b7a26326f09fda4dac26
mariogarzac <mariogarzac99@gmail.com>
2023-12-27 13:56:55 -0600

    Added models for response and file

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -8,9 +8,20 @@ type TemplateData struct {
 }
 
 type User struct {
-    UserID string `json:"userId"`
-    Name string `json:"name"`
-    Email string `json:"email"`
+    UserID string   `json:"userId"`
+    Name string     `json:"name"`
+    Email string    `json:"email"`
     Password string 
+}
+
+type File struct {
+    FileName string `json:"filename"`
+    FileType string `json:"filetype"`
+    FileSize int64  `json:"filesize"`
+}
+
+type Response struct {
+    Message string `json:"message"`
+    Data  File     `json:"file_info"`
 }
 
